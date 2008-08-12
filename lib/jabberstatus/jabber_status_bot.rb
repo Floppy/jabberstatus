c6a931c56bc9c3710f734efc955d3f831d0aca86
James Smith <james@floppy.org.uk>
2008-08-12 18:47:32 +0100

    don't add resource in script, it should just be set in config file

--- lib/jabberstatus/jabber_status_bot.rb
+++ lib/jabberstatus/jabber_status_bot.rb
@@ -23,7 +23,7 @@ class JabberStatusBot
     @service = ServiceFactory.create_service(options)
     # Create XMPP client
     @log.debug "Creating jabber client"
-    @client = Jabber::Client::new(options['jabber_id'] + '/JabberStatus')
+    @client = Jabber::Client::new(options['jabber_id'])
     @client.connect
     @client.auth(options['jabber_password'])
     @log.debug "Authenticated with Jabber server"
